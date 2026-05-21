13c373705f2356fc19f1dcef98872cc6ef70a5db
william-mad <will.123713@gmail.com>
2026-05-21 19:12:17 -0300

    hard

--- creature1.gd
+++ creature1.gd
@@ -1,40 +1,42 @@
-extends CharacterBody2D
+class_name Zenith extends CharacterBody2D
 
 
 @export var gravity: float = 1200.0
 @export var friction: float = 900.0
 @export var rope_anchor_strength: float = 1.0
 @export var rope_weight: float = 0.1
+@export var move_speed: float = 400
 
 @onready var sprite_2d: Sprite2D = %Sprite2D
 @onready var rope_attach_point: Marker2D = %RopeAttachPoint
 
 
-var direction : Vector2 = Vector2.ZERO
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var damage_area: Damage_Area = $Damage_Area
 
 
+var dir : float = 1.0
+var move_tween : Tween
+
+func _ready() -> void:
+	animation_player.play("walk")
+	pass
 
 func _physics_process(delta: float) -> void:
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	velocity.x = move_toward(velocity.x, 0.0, friction * delta)
+	if is_on_wall():
+		update_direction(-dir)
+	velocity += get_gravity() * delta
+	velocity.x = dir * move_speed
 
 	move_and_slide()
 	
 	
-func update_direction()->void:
-	
-	var previous_direction: Vector2 = direction
-	
-
-	
-	if previous_direction.x != direction.x:
-
-		if direction.x < 0:
-			sprite_2d.flip_h = true
-		elif direction.x > 0:
-			sprite_2d.flip_h = false
+func update_direction(new_dir : float)->void:
+	dir = new_dir
+	if dir < 0:
+		sprite_2d.flip_h = true
+	elif dir > 0:
+		sprite_2d.flip_h = false
 			
 	pass
 	
